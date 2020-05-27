1ac6b7f0a1b42075b818d23f8e061b4e8ad7f929
bartoszjarzab <bartosz.jarzab@gmail.com>
2020-05-27 14:35:26 +0200

    adding bootstrap for data. closes #14

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,11 +1,24 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.services.OwnerService;
+import com.springframework.sfgpetclinic.services.VetService;
+import com.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import com.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public OwnerController(){
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
     public String listOwners(){
         return "owners/index";
