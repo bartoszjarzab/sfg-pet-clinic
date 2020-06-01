81224c9ece67448b9c0e6c7c0dac3d6ad823ff1b
bartoszjarzab <bartosz.jarzab@gmail.com>
2020-06-01 15:30:00 +0200

    added project lombok to project. closes #51

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/model/Person.java
@@ -1,29 +1,31 @@
 package com.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
+
     @Column(name = "first_name")
     private String firstName;
 
     @Column(name="last_name")
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
+    public Person(Long id) {
+        super(id);
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName=firstName;
+        this.lastName=lastName;
     }
 }
