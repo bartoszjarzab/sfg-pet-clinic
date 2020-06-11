73ed02299e773644e0377052e704e00cc3b62fc1
bartoszjarzab <bartosz.jarzab@gmail.com>
2020-06-11 14:26:08 +0200

    added a rest endpoint for vets, closes #70

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,14 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,5 +23,9 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
